3ef4ba88e16e655f0866aca22233b4abddd66859
Zhazgul Arystanbek kyzy <zhazgul.arystanbekkyzy.kk@gmail.com>
2023-11-30 20:46:25 +0300

    added file day 11

--- challenge/day6/js.js
+++ challenge/day6/js.js
@@ -1,9 +1,22 @@
-let a = "Zhazgul Arstanbek";
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Apples":
+    console.log("Apples are $0.32 a pound.");
+    break;
+  case "Bananas":
+    console.log("Bananas are $0.48 a pound.");
+    break;
+  case "Cherries":
+    console.log("Cherries are $3.00 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    break;
+  default:
+    console.log("Sorry, we are out of " + expr + ".");
+}
 
-let b = a.split(" ");
-
-let c = b.map((el) => {
-  return el[0] + ".";
-});
-let d = c.join("");
-console.log(d);
+console.log("Is there anything else you'd like?");

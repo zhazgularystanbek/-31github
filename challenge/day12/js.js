b3e8451e62a0b4798aca64d1ec97e1ce882d445d
Zhazgul Arystanbek kyzy <zhazgul.arystanbekkyzy.kk@gmail.com>
2023-12-01 04:20:53 +0300

    addede day 12

---
+++ challenge/day12/js.js
@@ -0,0 +1,9 @@
+let arr = ["John", "Mike", "Andry", "alex"];
+arr.forEach((item, index) => {
+  console.log(`${index}: ${item}`);
+});
+
+let arr2 = ["Mike", "Ray", "Liam", "Mason", "Jacob"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
